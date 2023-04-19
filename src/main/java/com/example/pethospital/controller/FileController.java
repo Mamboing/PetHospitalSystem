be5d21acbb3085f587c24fa3e465adef45422004
Mamboing <51279227+Mamboing@users.noreply.github.com>
2023-04-19 14:05:52 +0800

    添加跨域处理

--- src/main/java/com/example/pethospital/controller/FileController.java
+++ src/main/java/com/example/pethospital/controller/FileController.java
@@ -19,6 +19,7 @@ import java.util.Date;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/file")
 @Slf4j
 public class FileController {
--- src/main/java/com/example/pethospital/controller/QuestionController.java
+++ src/main/java/com/example/pethospital/controller/QuestionController.java
@@ -14,6 +14,7 @@ import javax.annotation.Resource;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @Slf4j
 public class QuestionController {
     @Resource
